26bc87385bf6d6ba3bd99a4e36e583445bb2f603
Sergio Bonaque <s.bonaquecontacto@gmail.com>
2026-01-01 19:11:21 +0000

    alinear cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-light bg-danger text-white ">
+
             <a className="navbar-brand" href="#">
-                <img src="https://img.icons8.com/color/512/star-wars.png" width="30" height="30" className="d-inline-block align-top" alt="" />
-                StarWars
+                <img src="https://img.icons8.com/color/512/star-wars.png" width="80" height="80" className="d-inline-block align-top ms-4 " alt="" />
+            </a>
+            <a className="btn btn-primary bg-dark text-white rounded-pill border-0 me-4" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Favorites 0
             </a>
         </nav>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,41 @@ export const Home = () => {
 	const { store, dispatch } = useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
+		<div className="text-center mt-5 ">
 			<h1>Characters</h1>
-			<div className="card">
-				<img className="card-img-top" src=".../100px180/" alt="Card image cap"/>
+			<div className="d-flex flex-nowrap gap-3 justify-content-center">
+				<div className="card" style={{ width: "180px" }}>
+					<img className="card-img-top" src=".../100px180/" alt="" />
 					<div className="card-body">
-						<h5 className="card-title">...</h5>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" className="btn btn-primary">Go somewhere</a>
+						<h5 className="card-title">Luke Skywalker</h5>
+						<small className="d-block">Gender: Male</small>
+						<small className="d-block">Hair Color: blond</small>
+						<small className="d-block">Eye-color: blue</small>
+						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
 					</div>
+				</div>
+
+				<div className="card" style={{ width: "180px" }}>
+					<img className="card-img-top" src=".../100px180/" alt="" />
+					<div className="card-body">
+						<h5 className="card-title">Leia Organa</h5>
+						<small className="d-block">Gender: Female</small>
+						<small className="d-block">Hair Color: brown</small>
+						<small className="d-block">Eye-color: brown</small>
+						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+					</div>
+				</div>
+			</div>
+			<h1>Planets</h1>
+			<div className="card">
+				<img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+				<div className="card-body">
+					<h5 className="card-title">...</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
 			</div>
 		</div>
+
 	);
 }; 
