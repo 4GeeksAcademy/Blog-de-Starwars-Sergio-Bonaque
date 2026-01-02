670e088c0caffeeb0cdfd43a9bed7c51867c4a76
Sergio Bonaque <s.bonaquecontacto@gmail.com>
2026-01-02 16:28:10 +0000

    iconos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
+
 export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
@@ -10,35 +11,43 @@ export const Home = () => {
 			<h1>Characters</h1>
 			<div className="d-flex flex-nowrap gap-3 justify-content-center">
 				<div className="card">
-					<img className="card-img-top" src="https://cdn.zendalibros.com/wp-content/uploads/2025/01/luke-skaywalker-star-wars.jpg" alt=""  style={{ width: '172px', height: '150px' }}/>
+					<img className="card-img-top" src="https://cdn.zendalibros.com/wp-content/uploads/2025/01/luke-skaywalker-star-wars.jpg" alt="" style={{ width: '172px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Luke Skywalker</h5>
 						<small className="d-block">Gender: Male</small>
 						<small className="d-block">Hair Color: blond</small>
 						<small className="d-block">Eye-color: blue</small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
-						<i class="bi bi-hand-thumbs-up-fill"></i>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 
 				<div className="card">
-					<img className="card-img-top" src="https://www.ecured.cu/images/f/f3/Leia.jpg" alt="" style={{ width: '172px', height: '150px' }}/>
+					<img className="card-img-top" src="https://www.ecured.cu/images/f/f3/Leia.jpg" alt="" style={{ width: '172px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Leia Organa</h5>
 						<small className="d-block">Gender: Female</small>
 						<small className="d-block">Hair Color: brown</small>
 						<small className="d-block">Eye-color: brown</small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 				<div className="card">
-					<img className="card-img-top" src="https://static.wikia.nocookie.net/personajesaleatorios/images/2/2f/ThumbnailCA27HS8M.jpg/revision/latest/scale-to-width-down/300?cb=20120205225045&path-prefix=es" alt="" style={{ width: '178px', height: '150px' }}/>
+					<img className="card-img-top" src="https://static.wikia.nocookie.net/personajesaleatorios/images/2/2f/ThumbnailCA27HS8M.jpg/revision/latest/scale-to-width-down/300?cb=20120205225045&path-prefix=es" alt="" style={{ width: '178px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Obi-Wan Kenobi</h5>
 						<small className="d-block">Gender: Male</small>
 						<small className="d-block">Hair Color: blond</small>
 						<small className="d-block">Eye-color: brown</small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 				<div className="card">
@@ -49,40 +58,52 @@ export const Home = () => {
 						<small className="d-block">Hair Color: black</small>
 						<small className="d-block">Eye-color: brown</small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 				<div className="card">
-					<img className="card-img-top" src="https://sm.ign.com/t/ign_latam/screenshot/default/anakinsky1_7kyq.1280.jpg" alt="" style={{ width: '178px', height: '150px' }}/>
+					<img className="card-img-top" src="https://sm.ign.com/t/ign_latam/screenshot/default/anakinsky1_7kyq.1280.jpg" alt="" style={{ width: '178px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Anakin Skywalker</h5>
 						<small className="d-block">Gender: Male</small>
 						<small className="d-block">Hair Color: brown</small>
 						<small className="d-block">Eye-color: brown</small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 				<div className="card">
-					<img className="card-img-top" src="https://static.wikia.nocookie.net/starwars/images/1/1b/SwillaCorey1.jpg/revision/latest?cb=20050820090427" alt="" style={{ width: '178px', height: '150px' }}/>
+					<img className="card-img-top" src="https://static.wikia.nocookie.net/starwars/images/1/1b/SwillaCorey1.jpg/revision/latest?cb=20050820090427" alt="" style={{ width: '178px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Swilla Corey</h5>
 						<small className="d-block">Gender: Female</small>
 						<small className="d-block">Hair Color: blond</small>
 						<small className="d-block">Eye-color: brown</small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 				<div className="card">
-					<img className="card-img-top" src="https://static.wikia.nocookie.net/starwars/images/4/43/Tiree.png/revision/latest?cb=20130303144216" alt="" style={{ width: '178px', height: '150px' }}/>
+					<img className="card-img-top" src="https://static.wikia.nocookie.net/starwars/images/4/43/Tiree.png/revision/latest?cb=20130303144216" alt="" style={{ width: '178px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Gold Two</h5>
 						<small className="d-block">Gender: Male</small>
 						<small className="d-block">Hair Color: brown</small>
 						<small className="d-block">Eye-color: brown</small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 			</div>
-			
+
 			<h1>Planets</h1>
 			<div className="d-flex flex-nowrap gap-3 justify-content-center">
 				<div className="card">
@@ -93,50 +114,65 @@ export const Home = () => {
 						<small className="d-block">Element: Fire</small>
 						<small className="d-block"></small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 
 				<div className="card">
-					<img className="card-img-top" src="https://assets.science.nasa.gov/dynamicimage/assets/science/astro/exo-explore/internal_resources/121/Star_Wars_Kepler-22b_Kamino.png?w=1280&h=720&fit=clip&crop=faces%2Cfocalpoint" alt="" style={{ width: '172px', height: '150px' }}/>
+					<img className="card-img-top" src="https://assets.science.nasa.gov/dynamicimage/assets/science/astro/exo-explore/internal_resources/121/Star_Wars_Kepler-22b_Kamino.png?w=1280&h=720&fit=clip&crop=faces%2Cfocalpoint" alt="" style={{ width: '172px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Meet 8</h5>
 						<small className="d-block">Population:345.975</small>
 						<small className="d-block">Element: Water</small>
 						<small className="d-block"></small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 				<div className="card">
-					<img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/6/6d/Tatooine_%28fictional_desert_planet%29.jpg" alt="" style={{ width: '172px', height: '150px' }}/>
+					<img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/6/6d/Tatooine_%28fictional_desert_planet%29.jpg" alt="" style={{ width: '172px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Popon</h5>
 						<small className="d-block">Population:100.00</small>
 						<small className="d-block">Element: Air</small>
 						<small className="d-block"></small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 				<div className="card">
-					<img className="card-img-top" src="https://static0.srcdn.com/wordpress/wp-content/uploads/2023/10/major-star-wars-planets-future-image.jpg" alt="" style={{ width: '172px', height: '150px' }}/>
+					<img className="card-img-top" src="https://static0.srcdn.com/wordpress/wp-content/uploads/2023/10/major-star-wars-planets-future-image.jpg" alt="" style={{ width: '172px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Mahari</h5>
 						<small className="d-block">Population:200.700</small>
 						<small className="d-block">Element:Ground</small>
 						<small className="d-block"></small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
 				<div className="card">
-					<img className="card-img-top" src="https://cdna.artstation.com/p/assets/images/images/017/390/192/large/brian-hagan-pla-ossus-final.jpg?1555781865" alt="" style={{ width: '172px', height: '150px' }}/>
+					<img className="card-img-top" src="https://cdna.artstation.com/p/assets/images/images/017/390/192/large/brian-hagan-pla-ossus-final.jpg?1555781865" alt="" style={{ width: '172px', height: '150px' }} />
 					<div className="card-body">
 						<h5 className="card-title">Pepique</h5>
 						<small className="d-block">Population 350.000</small>
 						<small className="d-block">Element: God</small>
 						<small className="d-block"></small>
 						<a href="#" className="btn btn-primary btn-sm mt-2">Learn More!</a>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-hand-thumbs-up-fill ms-3" viewBox="0 0 16 16">
+							<path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+						</svg>
 					</div>
 				</div>
-				
+
 			</div>
 		</div>
 
