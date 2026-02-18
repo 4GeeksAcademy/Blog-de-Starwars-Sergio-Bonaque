3d9888ac73b75654016ee49fa3bddea99c2a1d8d
Sergio Bonaque <s.bonaquecontacto@gmail.com>
2026-02-18 12:07:53 +0000

    arreglos

--- src/store.js
+++ src/store.js
@@ -1,44 +1,24 @@
 export const initialStore = () => {
-  return {
-    message: null,
-    favorites: [],
-    todos: [
-      { id: 1, title: "Make the bed", background: null },
-      { id: 2, title: "Do my homework", background: null }
-    ]
-  };
+    return {
+        favorites: []
+    };
 };
 
 export default function storeReducer(store, action = {}) {
-  switch (action.type) {
-    case "add_task": {
-      const { id, color } = action.payload;
-      return {
-        ...store,
-        todos: store.todos.map((todo) =>
-          todo.id === id ? { ...todo, background: color } : todo
-        ),
-      };
+    switch (action.type) {
+        case "ADD_FAVORITE":
+            return {
+                ...store,
+                favorites: [...store.favorites, action.payload]
+            };
+        case "REMOVE_FAVORITE":
+            return {
+                ...store,
+                favorites: store.favorites.filter(fav => 
+                    !(fav.id === action.payload.id && fav.type === action.payload.type)
+                )
+            };
+        default:
+            return store;
     }
-
-    case "ADD_FAVORITE": {
-      return {
-        ...store,
-        favorites: [...store.favorites, action.payload],
-      };
-    }
-
-    case "REMOVE_FAVORITE": {
-      const { id, type } = action.payload;
-      return {
-        ...store,
-        favorites: store.favorites.filter(
-          (fav) => !(fav.id === id && fav.type === type)
-        ),
-      };
-    }
-
-    default:
-      throw new Error("Unknown action.");
-  }
 }
